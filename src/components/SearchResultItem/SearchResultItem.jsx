3169b2a0ab12ce540c275f4b95337a4142058271
whoguy24 <whoguy24@gmail.com>
2021-12-10 10:13:23 -0600

    Created addToFavorites button.

--- src/components/SearchResultItem/SearchResultItem.jsx
+++ src/components/SearchResultItem/SearchResultItem.jsx
@@ -1,9 +1,28 @@
+import { useDispatch } from 'react-redux';
+
 function SearchResultItem({ result }) {
+
+    const dispatch = useDispatch();
+
+    // Function to add searchItemResult/Giphy object to favorites reducer
+    function addToFavorites () {
+        console.log('Adding to favorites:', result);
+        dispatch({
+            type: 'NEW_FAVORITES',
+            payload: result
+        })
+    }
+
+    // Render Component on the DOM
     return (
         <div style={{display: 'inline-block'}}>
             <img src={result.images.fixed_height.url}></img>
+            <div>
+                <button onClick = { () => {addToFavorites()}}>Add to Favorites</button>
+            </div>
         </div>
     )
 }
 
+// Export Component
 export default SearchResultItem;
